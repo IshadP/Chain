1e260896c8c2f54cad9f6782b32f6f97d586b24f
Ishad Pande <ishadpande16@gmail.com>
2025-09-15 18:20:48 +0530

    "some backend manifest files updated; will implement fetching of blockchain data"

--- apps/web/.next/types/validator.ts
+++ apps/web/.next/types/validator.ts
@@ -101,15 +101,6 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
-// Validate ../../src/app/api/batches/[batchId]/route.ts
-{
-  type __IsExpected<Specific extends RouteHandlerConfig<"/api/batches/[batchId]">> = Specific
-  const handler = {} as typeof import("../../src/app/api/batches/[batchId]/route.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../src/app/api/batches/[batchId]/transfer/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/batches/[batchId]/transfer">> = Specific
